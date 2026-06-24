5e045e930743fcce7b3ae43615e53bde915a81f8
Henzo Brito Dos Santos <henzo.2025@proton.me>
2026-06-23 21:24:34 -0300

    v-1.0.0

--- src/projects/G.js
+++ src/projects/G.js
@@ -2,14 +2,14 @@ import { drawProjectHtml } from "../tools/drawProjectHTML.js"
 import { isNumber } from "../tools/isNumber.js"
 import { sendANewResponse } from "../tools/response.js"
 
-import { createTheProjectB } from "./B.js"
+import { createTheProjectH } from "./H.js"
 
 export function createTheProjectG(){
     drawProjectHtml(
       "G", 
-      "Este programa lê quatro valores numéricos inteiros e apresentar o resultado de todas as multiplicações e adições.", 
+      "Este programa lê quatro valores inteiros e apresenta o resultado de todas as adições e multiplicações possíveis entre as variáveis A, B, C e D.", 
       4, 
-      ["Inserir o valor(A)", "Inserir o valor(B)", "Inserir o valor(C)", "Inserir o valor(D)"], 
+      ["Inserir o valor (A)", "Inserir o valor (B)", "Inserir o valor (C)", "Inserir o valor (D)"], 
       true
     )
 
@@ -20,55 +20,46 @@ export function createTheProjectG(){
     const input3 = document.getElementById("input2")
     const input4 = document.getElementById("input3")
 
-    button.addEventListener("click",()=>{
-        if(input1.value && input2.value && input3.value && input4.value){
-            let inputs = [
-                {
-                    id: "A",
-                    number: isNumber(input1.value),
-                },
-                {
-                    id: "B",
-                    number: isNumber(input2.value),
-                },
-                {
-                    id: "C",
-                    number: isNumber(input3.value),
-                },
-                {
-                    id: "D",
-                    number: isNumber(input4.value),
-                }
-            ]
+    button.addEventListener("click", () => {
+        if (input1.value && input2.value && input3.value && input4.value) {
+            const A = isNumber(input1.value)
+            const B = isNumber(input2.value)
+            const C = isNumber(input3.value)
+            const D = isNumber(input4.value)
 
-            let listenedInputs = [
-                {
-                    ids: ["A","B"],
-                    values: [m, s] 
-                }
-            ]
+            if (A.boolNumber && B.boolNumber && C.boolNumber && D.boolNumber) {
+                const a = A.valueNumber
+                const b = B.valueNumber
+                const c = C.valueNumber
+                const d = D.valueNumber
 
-            for(let input of inputs){
-                if(input.number.boolNumber){
+                sendANewResponse(
+                    "Resposta",
+                    `
+                        Adições:
+                        A + B = ${a + b};\n
+                        A + C = ${a + c};\n
+                        A + D = ${a + d};\n
+                        B + C = ${b + c};\n
+                        B + D = ${b + d};\n
+                        C + D = ${c + d};\n
 
-                    console.log(input)
-    
-                    sendANewResponse(
-                        "Resposta", 
-                        `
-                            Valor de A: ${A.valueNumber}
-                            Valor de B: ${B.valueNumber}
-                        `,
-                        "var(--color2)",    
-                        createTheProjectB,
-                        "Projeto F",    
-                        false
-                    )
-                }
+                        Multiplicações:
+                        A × B = ${a * b};\n
+                        A × C = ${a * c};\n
+                        A × D = ${a * d};\n
+                        B × C = ${b * c};\n
+                        B × D = ${b * d};\n
+                        C × D = ${c * d};\n
+                    `,
+                    "var(--color2)",
+                    createTheProjectH,
+                    "Projeto H",
+                    false
+                )
             }
-
-        }else{
-            sendANewResponse("Erro", "Por favor adicione um valor corrreto", "var(--color4)")
+        } else {
+            sendANewResponse("Erro", "Por favor adicione um valor correto", "var(--color4)")
         }
     })
 }
